744c455eeabf4bf50dabe9c194c6da47f796402a
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2023-01-01 17:48:40 -0600

    up

--- registration-from/src/App.tsx
+++ registration-from/src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import "./index.css";
 
-
-function App() {
+export default function App() {
   return (
-    <div className="App">kill me
+    <div className="form-container">
+      <form className="register-form">
+        <input
+          id="first-name"
+          className="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+        />
+        <input
+          id="last-name"
+          className="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+        />
+        <input
+          id="email"
+          className="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+        <button className="form-field" type="submit">
+          Register
+        </button>
+      </form>
     </div>
   );
 }
-
-export default App;

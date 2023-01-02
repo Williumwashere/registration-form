fe588dc76b844fe3722ca88397a6bc11f92d24e7
Williumwashere <61668736+Williumwashere@users.noreply.github.com>
2023-01-01 18:23:09 -0600

    Update App.tsx

--- registration-from/src/App.tsx
+++ registration-from/src/App.tsx
@@ -1,17 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 export default function App() {
-  const [values, setvalue] = useState({
+  const [values, setValues] = useState({
     firstName: "",
     lastName: "",
     email: "",
   });
+
+  const handleFirstNameInput = (event:any) => {
+    setValues({ ...values, firstName: event.target.value });
+  };
+
+  const handleLastNameInput = (event:any) => {
+    setValues({ ...values, lastName: event.target.value });
+  };
+
+  const handleEmailInput = (event:any) => {
+    setValues({...values, lastName: event.target.value})
+  }
+
   return (
     <div className="form-container">
       <form className="register-form">
         <input
           value={values.firstName}
+          onChange={handleFirstNameInput}
           id="first-name"
           className="form-field"
           type="text"
@@ -20,6 +34,7 @@ export default function App() {
         />
         <input
           value={values.lastName}
+          onChange={handleLastNameInput}
           id="last-name"
           className="form-field"
           type="text"
@@ -28,6 +43,7 @@ export default function App() {
         />
         <input
           value={values.email}
+          onChange={handleEmailInput}
           id="email"
           className="form-field"
           type="text"
